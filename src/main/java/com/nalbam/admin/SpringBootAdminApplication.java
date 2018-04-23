3606eddd916fc45211916b57be7e629052cd4b72
nalbam <me@nalbam.com>
2018-04-23 18:52:32 +0900

    auth

--- src/main/java/com/nalbam/admin/SpringBootAdminApplication.java
+++ src/main/java/com/nalbam/admin/SpringBootAdminApplication.java
@@ -3,9 +3,10 @@ package com.nalbam.admin;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
-//@EnableDiscoveryClient // Don't client
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootAdminApplication {
 
